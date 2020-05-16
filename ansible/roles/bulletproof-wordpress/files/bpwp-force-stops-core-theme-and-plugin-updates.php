5a7a6ce0314dc731659bcadec88feabd69486521
Daniel Kuruc <daniel@kuruc.dev>
2020-05-16 18:11:20 +0200

    Bugfix mu-plugins

--- ansible/roles/bulletproof-wordpress/files/bpwp-force-stops-core-theme-and-plugin-updates.php
+++ ansible/roles/bulletproof-wordpress/files/bpwp-force-stops-core-theme-and-plugin-updates.php
@@ -1,31 +1,27 @@
 <?php
 
-add_action('init', 'bpwp_force_easyupdatesmanager', 11);
-add_action('pre_current_active_plugins', 'bpwp_hide_easyupdatesmanager', 0);
+add_action('pre_current_active_plugins', function () {
+    global $wp_list_table;
 
-define('BPWP_PLUGIN_EASYUPDATESMANAGER', 'stops-core-theme-and-plugin-updates/main.php');
+    $PLUGIN = 'stops-core-theme-and-plugin-updates/main.php';
 
-function bpwp_force_easyupdatesmanager() {
     # Make sure plugin is always active
-    activate_plugin(BPWP_PLUGIN_EASYUPDATESMANAGER);
+    activate_plugin($PLUGIN);
+
     # Hide option for deactivating plugin
-    add_filter('plugin_action_links', function ($actions, $plugin_file) {
-        if (array_key_exists('deactivate', $actions) && $plugin_file === BPWP_PLUGIN_EASYUPDATESMANAGER) {
+    add_filter('plugin_action_links', function ($actions, $plugin_file) use ($PLUGIN) {
+        if (array_key_exists('deactivate', $actions) && $plugin_file === $PLUGIN) {
             unset($actions['deactivate']);
         }
         return $actions;
     }, 10, 2);
-}
 
-# Hide plugin from installed plugins list
-function bpwp_hide_easyupdatesmanager() {
-    global $wp_list_table;
-
+    # Hide plugin from installed plugins list
     $myplugins = $wp_list_table->items;
     foreach ($myplugins as $key => $val) {
-        if ($key === BPWP_PLUGIN_EASYUPDATESMANAGER) {
+        if ($key === $PLUGIN) {
             unset($wp_list_table->items[$key]);
             break;
         }
     }
-}
+}, 0);
--- ansible/roles/bulletproof-wordpress/files/bpwp-force-wp-mail-smtp.php
+++ ansible/roles/bulletproof-wordpress/files/bpwp-force-wp-mail-smtp.php
@@ -1,31 +1,27 @@
 <?php
 
-add_action('init', 'bpwp_force_wpmailsmtp', 11);
-add_action('pre_current_active_plugins', 'bpwp_hide_wpmailsmtp', 0);
+add_action('pre_current_active_plugins', function () {
+    global $wp_list_table;
 
-define('BPWP_PLUGIN_WPMAILSMTP', 'wp-mail-smtp/wp_mail_smtp.php');
+    $PLUGIN = 'wp-mail-smtp/wp_mail_smtp.php';
 
-function bpwp_force_wpmailsmtp() {
     # Make sure plugin is always active
-    activate_plugin(BPWP_PLUGIN_WPMAILSMTP);
+    activate_plugin($PLUGIN);
+
     # Hide option for deactivating plugin
-    add_filter('plugin_action_links', function ($actions, $plugin_file) {
-        if (array_key_exists('deactivate', $actions) && $plugin_file === BPWP_PLUGIN_WPMAILSMTP) {
+    add_filter('plugin_action_links', function ($actions, $plugin_file) use ($PLUGIN) {
+        if (array_key_exists('deactivate', $actions) && $plugin_file === $PLUGIN) {
             unset($actions['deactivate']);
         }
         return $actions;
     }, 10, 2);
-}
 
-# Hide plugin from installed plugins list
-function bpwp_hide_wpmailsmtp() {
-    global $wp_list_table;
-
+    # Hide plugin from installed plugins list
     $myplugins = $wp_list_table->items;
     foreach ($myplugins as $key => $val) {
-        if ($key === BPWP_PLUGIN_WPMAILSMTP) {
+        if ($key === $PLUGIN) {
             unset($wp_list_table->items[$key]);
             break;
         }
     }
-}
+}, 0);
